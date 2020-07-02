4d4eeb21486a7d6df306cbc56d270bdaa6256aef
gersini <gerson.choque.loreno@gmial.com>
2020-07-02 15:40:16 -0400

    requisitos front end

--- src/app/components/configurar-convocatoria/requerimientos/requerimientos.component.ts
+++ src/app/components/configurar-convocatoria/requerimientos/requerimientos.component.ts
@@ -9,7 +9,12 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';//formulario
 })
 export class RequerimientosComponent implements OnInit {
   formulario: FormGroup;
-
+  listaItemsBD: any[][] = [];
+  listaItemsDisponible:any[][] = [];
+  listaItemsSeleccionados:any[][] = [];
+  banderaAdd:boolean;
+  idItemEditar:string;
+  
   constructor() { }
 
   ngOnInit(): void {  
@@ -17,61 +22,210 @@ export class RequerimientosComponent implements OnInit {
       tipoDeConvocatoria: new FormControl('', [Validators.required]), //[Validators.required, Validators.minLength(2)]
       numeroDeItems: new FormControl('', [Validators.required]),
       horasAcademicas: new FormControl('',[Validators.required])
-    });
+    });        
+    this.listaItemsBD = [["1","7473802","Introduccion a la programacion"],
+                         ["2","7897155","Elementos de programacion y estructura de datos"],
+                         ["3","2685656","Teoria de grafos"],
+                         ["4","7882145","Computacion"]];
+    
+    this.listaItemsDisponible = this.listaItemsBD.slice();//copy    
+    this.banderaAdd = true;
+    this.idItemEditar = "-1";
   }
 
-  enviarFormulario(){
+  activarbanderaAddItem(){
+    this.banderaAdd = true;
+  }
+
+  desactivarbanderaAddItem(){
+    this.banderaAdd = false;
+  }
+
+  addItem(){
     var botonEnviar = document.getElementById("botonEnviar");
     botonEnviar.removeAttribute("data-dismiss");
 
-    if(this.validarFormulario()){
-      console.log("puedo enviar");
-      this.limpiarFormulario();
-            
+    if(this.validarFormulario(true)){
+      console.log("puedo añadir a la tabla");
+
+      var idItem = (<HTMLInputElement>document.getElementById('tipoDeConvocatoria')).value;
+      var numItems = (<HTMLInputElement>document.getElementById('numeroDeItems')).value;
+      var numItemsAdHonorem = (<HTMLInputElement>document.getElementById('numeroDeItemsAdHonorem')).value;
+      var hrsAcademicas = (<HTMLInputElement>document.getElementById('horasAcademicas')).value;
+      
+      let indexItem = this.getIndexDelItem(idItem,this.listaItemsDisponible);
+      var lista:any[] = [];
+      
+      lista.push(this.listaItemsDisponible[indexItem][0]);//idItem
+      lista.push(this.listaItemsDisponible[indexItem][2]);//nombre
+      lista.push(numItems);
+      lista.push(numItemsAdHonorem);
+      lista.push(hrsAcademicas);
+      
+      console.log("indice: " + indexItem);
+
+      this.listaItemsSeleccionados.push(lista);      
+      this.listaItemsDisponible.splice(indexItem,1);//delete
+      
       botonEnviar.setAttribute("data-dismiss","modal");
     }else{
       console.log("no puedo enviar");
     }
   }
 
+  editarItem():void{
+    console.log("editar item: " + this.idItemEditar );
+    var botonActualizar = document.getElementById("botonActualizar");    
+    botonActualizar.removeAttribute("data-dismiss");
+
+    if(this.validarFormulario(false)){
+      let index = this.getIndexDelItem(this.idItemEditar,this.listaItemsSeleccionados);
+      let numItems = (<HTMLInputElement>document.getElementById('numeroDeItems')).value;
+      let numItemsAdHonorem = (<HTMLInputElement>document.getElementById('numeroDeItemsAdHonorem')).value;
+      let hrsAcademicas = (<HTMLInputElement>document.getElementById('horasAcademicas')).value;
+
+      this.listaItemsSeleccionados[index][2] = numItems;
+      this.listaItemsSeleccionados[index][3] = numItemsAdHonorem;
+      this.listaItemsSeleccionados[index][4] = hrsAcademicas;
+
+      botonActualizar.setAttribute("data-dismiss","modal");
+    }
+  }
+
+  setIdItemEditar(idItem:string){
+    this.idItemEditar = idItem;
+    this.limpiarFormulario();
+    this.desactivarListaItemsDisponibles()
+  } 
+
+  eliminarItem(idItem:string):void{
+    let index = this.getIndexDelItem(idItem,this.listaItemsSeleccionados);
+    this.listaItemsSeleccionados.splice(index,1);
+    
+    index = this.getIndexDelItem(idItem,this.listaItemsBD);
+    let lista:string[] = [];
+
+    lista.push(this.listaItemsBD[index][0]);
+    lista.push(this.listaItemsBD[index][1]);
+    lista.push(this.listaItemsBD[index][2]);
+    
+    this.listaItemsDisponible.push(lista);
+  }
+
+  getIndexDelItem(idItem:string,lista:string[][]):number{
+    let index = -1;
+    for (let i = 0; i < lista.length; i++) {
+      if(lista[i][0] === idItem){
+        index = i;
+        break;
+      }
+    }
+    return index;
+  }
+
   limpiarFormulario():void{
-    this.formulario.reset();
+    this.formulario.reset();    
+    
     var inputItem = document.getElementById('tipoDeConvocatoria');
     var inputNumItems = document.getElementById('numeroDeItems');
+    var inputNumItemsAdHonorem = document.getElementById('numeroDeItemsAdHonorem');
     var inputHrsAcademicas = document.getElementById('horasAcademicas');
 
     inputItem.classList.remove('is-invalid');
     inputNumItems.classList.remove('is-invalid');
+    inputNumItemsAdHonorem.classList.remove('is-invalid');
     inputHrsAcademicas.classList.remove('is-invalid');
 
     inputItem.classList.remove('is-valid');
     inputNumItems.classList.remove('is-valid');
+    inputNumItemsAdHonorem.classList.remove('is-valid');
     inputHrsAcademicas.classList.remove('is-valid');
   }
 
-  validarFormulario():boolean{    
+  validarSeleccionItem():void{
+    var inputItem = document.getElementById('tipoDeConvocatoria');
+    var item = (<HTMLInputElement>inputItem).value;
+    if(item !== ""){
+      inputItem.classList.remove('is-invalid');
+      inputItem.classList.add('is-valid');
+    }else{
+      inputItem.classList.remove('is-valid');
+      inputItem.classList.add('is-invalid');
+    }
+  }
+
+  validarNumeroDeItemsTitular():void{    
+    var inputNumItems = document.getElementById('numeroDeItems');    
+    var numItems = (<HTMLInputElement>inputNumItems).value;
+
+    if(this.esNumeroEntero(numItems)){      
+      inputNumItems.classList.remove('is-invalid');
+      inputNumItems.classList.add('is-valid');
+    }else{
+      inputNumItems.classList.remove('is-valid');
+      inputNumItems.classList.add('is-invalid');
+    }
+  }
+
+  validarNumeroDeItemsAdHonorem():void{    
+    var inputNumItems = document.getElementById('numeroDeItemsAdHonorem');    
+    var numItems = (<HTMLInputElement>inputNumItems).value;
+
+    if(this.esNumeroEntero(numItems)){      
+      inputNumItems.classList.remove('is-invalid');
+      inputNumItems.classList.add('is-valid');
+    }else{
+      inputNumItems.classList.remove('is-valid');
+      inputNumItems.classList.add('is-invalid');
+    }
+  }
+
+  validarHorasAcademicas():void{
+    var inputHrsAcademicas = document.getElementById('horasAcademicas');
+    var hrsAcademicas = (<HTMLInputElement>inputHrsAcademicas).value;
+    if(this.esNumeroEnteroOrDecimal(hrsAcademicas)){
+      inputHrsAcademicas.classList.remove('is-invalid');
+      inputHrsAcademicas.classList.add('is-valid');
+    }else{
+      inputHrsAcademicas.classList.remove('is-valid');
+      inputHrsAcademicas.classList.add('is-invalid');
+    }
+  }
+
+  validarFormulario(todosLosCampos:boolean):boolean{    
     var valido:boolean = false;
 
-    var form = document.getElementById("formulario");
     var inputItem = document.getElementById('tipoDeConvocatoria');
     var inputNumItems = document.getElementById('numeroDeItems');
+    var inputNumItemsAdHonorem = document.getElementById('numeroDeItemsAdHonorem');
     var inputHrsAcademicas = document.getElementById('horasAcademicas');
     var item = (<HTMLInputElement>inputItem).value;
     var numItems = (<HTMLInputElement>inputNumItems).value;
+    var numItemsAdHonorem = (<HTMLInputElement>inputNumItemsAdHonorem).value;
     var hrsAcademicas = (<HTMLInputElement>inputHrsAcademicas).value;
     
     
     
-    if(this.esNumeroEntero(numItems) && this.esNumeroEnteroOrDecimal(hrsAcademicas) && item !== "" ){   
-      inputItem.classList.remove('is-invalid');
-      inputNumItems.classList.remove('is-invalid');
-      inputHrsAcademicas.classList.remove('is-invalid');
+    if(this.esNumeroEntero(numItems) && this.esNumeroEntero(numItemsAdHonorem) && this.esNumeroEnteroOrDecimal(hrsAcademicas)){      
+        //inputItem.classList.remove('is-invalid');
+        inputNumItems.classList.remove('is-invalid');
+        inputNumItemsAdHonorem.classList.remove('is-invalid');
+        inputHrsAcademicas.classList.remove('is-invalid');
 
-      inputItem.classList.add('is-valid');
-      inputNumItems.classList.add('is-valid');
-      inputHrsAcademicas.classList.add('is-valid');
+        //inputItem.classList.add('is-valid');
+        inputNumItems.classList.add('is-valid');
+        inputNumItemsAdHonorem.classList.remove('is-valid');
+        inputHrsAcademicas.classList.add('is-valid');
 
-      valido = true;
+        valido = true;
+        if(todosLosCampos){
+          if(item !== ""){
+            inputItem.classList.remove('is-invalid');
+            inputItem.classList.add('is-valid');
+          }else{
+            valido = false;
+          }
+        }        
     }else{
       if(!this.esNumeroEntero(numItems)){
         inputNumItems.classList.remove('is-valid');
@@ -79,6 +233,14 @@ export class RequerimientosComponent implements OnInit {
       }else{
         inputNumItems.classList.remove('is-invalid');
         inputNumItems.classList.add('is-valid')
+      }
+
+      if(!this.esNumeroEntero(numItemsAdHonorem)){
+        inputNumItemsAdHonorem.classList.remove('is-valid');
+        inputNumItemsAdHonorem.classList.add('is-invalid');
+      }else{
+        inputNumItemsAdHonorem.classList.remove('is-invalid');
+        inputNumItemsAdHonorem.classList.add('is-valid')
       }
 
       if(!this.esNumeroEnteroOrDecimal(hrsAcademicas)){
@@ -89,12 +251,14 @@ export class RequerimientosComponent implements OnInit {
         inputHrsAcademicas.classList.add('is-valid');
       }
 
-      if(item===""){
-        inputItem.classList.remove('is-valid');
-        inputItem.classList.add('is-invalid');
-      }else{
-        inputItem.classList.remove('is-invalid');
-        inputItem.classList.add('is-valid');
+      if(todosLosCampos){
+        if(item===""){
+          inputItem.classList.remove('is-valid');
+          inputItem.classList.add('is-invalid');
+        }else{
+          inputItem.classList.remove('is-invalid');
+          inputItem.classList.add('is-valid');
+        }
       }
     }  
     return valido;
@@ -121,10 +285,20 @@ export class RequerimientosComponent implements OnInit {
     var esNumero = false;
     if(!this.textoVacio(num)){
       var n = Number(num);    
-      if(!isNaN(n)){ // es numero
+      if(!isNaN(n)){ //es numero
         esNumero = true;
       }
     }
     return esNumero;
+  }
+
+  activarListaItemsDisponibles(){
+    const nombreItem = document.getElementById("nombreItem");
+    nombreItem.removeAttribute("disabled");
+  }
+
+  desactivarListaItemsDisponibles(){
+    const nombreItem = document.getElementById("nombreItem");
+    nombreItem.setAttribute("disabled","");
   }
 }
--- src/app/components/configurar-convocatoria/requisitos/requisitos.component.ts
+++ src/app/components/configurar-convocatoria/requisitos/requisitos.component.ts
@@ -1,6 +1,8 @@
 import { element } from 'protractor';
 import { Component, OnInit } from '@angular/core';
 import { AnyARecord } from 'dns';
+import { FormGroup, FormControl, Validators } from '@angular/forms';//formularios , formularios reactivos, validadores de formularios
+
 
 @Component({
   selector: 'app-requisitos',
@@ -8,23 +10,112 @@ import { AnyARecord } from 'dns';
   styleUrls: ['./requisitos.component.css']
 })
 export class RequisitosComponent implements OnInit {
+  formulario: FormGroup;
+  listaRequisitosSeleccionados:string[][] = [];
+  banderaAdd:boolean;
+  idLista:number;
+  idrequisitoEditar:string;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.formulario = new FormGroup({
+      areaRequisito: new FormControl('', [Validators.required])
+    });
+    this.banderaAdd = true;
+    this.idLista = 1;
   }
 
-  add(event:any){
-    //alert("carajo" + event.target.textContent);
 
-    console.log(event);
+  addRequisito(){
+    let botonAdd = document.getElementById("botonAdd");    
+    botonAdd.removeAttribute("data-dismiss");
+
+    this.banderaAdd = true;
+    let requisito = document.getElementById('areaDeRequisito');
+    let requisitoText = (<HTMLInputElement>requisito).value;
+    if(this.esTextoValido(requisitoText)){
+      requisito.classList.add('is-valid');
+      let listaAux:string[] = [];
+      listaAux.push(this.idLista+"");
+      listaAux.push(requisitoText);
+      this.listaRequisitosSeleccionados.push(listaAux); 
+      this.idLista += 1;
+      
+      
+      botonAdd.setAttribute("data-dismiss","modal");
+    }
+  }
+
+  editarRequisito(){
+    this.banderaAdd = false;
+    let botoUpdate = document.getElementById("botonUpdate");    
+    botoUpdate.removeAttribute("data-dismiss");
+
+    if(this.validarAreaRequisito()){
+      let index = this.getIndex(this.idrequisitoEditar,this.listaRequisitosSeleccionados);
+      let requisitoText = (<HTMLInputElement>document.getElementById('areaDeRequisito')).value;
+      this.listaRequisitosSeleccionados[index][1] = requisitoText;
+
+      botoUpdate.setAttribute("data-dismiss","modal");
+    }
+  }
+
+  validarAreaRequisito():boolean{
+    let bandera:boolean;
+    let requisito = document.getElementById('areaDeRequisito');
+    let requisitoText = (<HTMLInputElement>requisito).value;
+
+    requisito.classList.remove('is-valid');
+    requisito.classList.remove('is-invalid');
     
-    
-    const el: HTMLElement = document.getElementById('moco');
-    el.setAttribute("class","bg-primary");
-    
-    var ss = el.getAttribute("id");
-    alert(ss);
-    console.log(el);
+    if(this.esTextoValido(requisitoText)){
+      requisito.classList.add('is-valid');
+      bandera = true;
+    }else{
+      requisito.classList.add('is-invalid');
+      bandera = false;
+    }
+    return bandera;
+  } 
+
+  esTextoValido(valor:any):boolean{
+    return !(valor == null || valor.length == 0 || /^\s+$/.test(valor));
+  }
+
+  limpiarFormulario(){
+    this.formulario.reset();
+    var requisito = document.getElementById('areaDeRequisito');
+
+    requisito.classList.remove('is-invalid');
+    requisito.classList.remove('is-valid');
+  }
+
+  setIdRequisitoEditar(idRequisito:string){
+    this.idrequisitoEditar = idRequisito;
+  }
+
+  eliminarRequisito(idRequisito:string){
+    let index = this.getIndex(idRequisito,this.listaRequisitosSeleccionados);
+    this.listaRequisitosSeleccionados.splice(index,1);
+  }
+
+  desactivarbanderaAddRequerimiento(){
+    this.banderaAdd = false;
+  }
+
+  activarbanderaAddRequerimiento(){
+    this.banderaAdd = true;
+  }
+
+  getIndex(idRequisito:string,lista:string[][]):number{
+    let index = -1;
+    for (let i = 0; i < lista.length; i++) {
+      if(lista[i][0]===idRequisito){
+        index = i;
+        break;
+      }      
+    }
+    return index;
   }
 }
